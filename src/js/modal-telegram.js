0900e557ff36174dd969b2b2f21ea87ae2da1b66
mikegurak <mykhailohurak@gmail.com>
2022-10-13 14:20:04 +0300

    update
    - added modals for 3 buttons in programs section

--- src/js/modal-telegram.js
+++ src/js/modal-telegram.js
@@ -2,15 +2,13 @@ const TOKEN = "5747023535:AAHZqWUgYIO9mprxjGZVMpq59VcQiIZrx-E";
 const CHATID = "-1001796692586";
 const URL_API = `https://api.telegram.org/bot${ TOKEN }/sendMessage`;
 
-document.getElementById('tg').addEventListener('submit', function (event) {
+document.getElementById('form-hero').addEventListener('submit', function (event) {
     event.preventDefault();
     
     let message = `<b>Кнопка - Головний екран</b>\n`;
     message += `<b>Ім'я: ${this.name.value}</b>\n`;
     message += `<b>Телефон: ${this.tel.value}</b>`;
-
-    // console.log(message);
-    
+ 
     axios.post(URL_API, {
         chat_id: CHATID,
         parse_mode: 'html',
@@ -22,5 +20,68 @@ document.getElementById('tg').addEventListener('submit', function (event) {
         })
         .catch((error) => {
             console.warn(error);
+        })
+});
+
+document.getElementById('form-programs1').addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    let message = `<b>Кнопка - Програма 1</b>\n`;
+    message += `<b>Ім'я: ${this.name.value}</b>\n`;
+    message += `<b>Телефон: ${this.tel.value}</b>`;
+ 
+    axios.post(URL_API, {
+        chat_id: CHATID,
+        parse_mode: 'html',
+        text: message
     })
-})
+        .then((response) => {
+            this.name.value = "";
+            this.tel.value = "";
+        })
+        .catch((error) => {
+            console.warn(error);
+        })
+});
+
+document.getElementById('form-programs2').addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    let message = `<b>Кнопка - Програма 2</b>\n`;
+    message += `<b>Ім'я: ${this.name.value}</b>\n`;
+    message += `<b>Телефон: ${this.tel.value}</b>`;
+ 
+    axios.post(URL_API, {
+        chat_id: CHATID,
+        parse_mode: 'html',
+        text: message
+    })
+        .then((response) => {
+            this.name.value = "";
+            this.tel.value = "";
+        })
+        .catch((error) => {
+            console.warn(error);
+        })
+});
+
+document.getElementById('form-programs3').addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    let message = `<b>Кнопка - Програма 2</b>\n`;
+    message += `<b>Ім'я: ${this.name.value}</b>\n`;
+    message += `<b>Телефон: ${this.tel.value}</b>`;
+ 
+    axios.post(URL_API, {
+        chat_id: CHATID,
+        parse_mode: 'html',
+        text: message
+    })
+        .then((response) => {
+            this.name.value = "";
+            this.tel.value = "";
+        })
+        .catch((error) => {
+            console.warn(error);
+        })
+});
