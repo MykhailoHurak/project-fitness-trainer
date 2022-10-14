0e356f3f3864ea5513e3a2a75fdc6168c4e71568
MykhailoHurak <mykhailohurak@gmail.com>
2022-10-14 10:36:19 +0000

    Deploying to gh-pages from @ MykhailoHurak/project-fitness-trainer@c35b320f5b3bdd57e1e381aa5a6f490439d15548 🚀

--- index.0ca07c85.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-programs3-open]"),closeModalBtn:document.querySelector("[data-modal-programs3-close]"),modal:document.querySelector("[data-modal-programs3]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.0ca07c85.js.map
--- index.6c547a8a.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-programs1-open]"),closeModalBtn:document.querySelector("[data-modal-programs1-close]"),modal:document.querySelector("[data-modal-programs1]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.6c547a8a.js.map
