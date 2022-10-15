94d30a24a278d1bb7ae0aa1a641f5d5824f69e22
mikegurak <mykhailohurak@gmail.com>
2022-10-15 22:48:25 +0300

    update

--- src/js/modal-telegram.js
+++ src/js/modal-telegram.js
@@ -1,9 +1,15 @@
 const TOKEN = "5747023535:AAHZqWUgYIO9mprxjGZVMpq59VcQiIZrx-E";
 const CHATID = "-1001796692586";
-const URL_API = `https://api.telegram.org/bot${ TOKEN }/sendMessage`;
+const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+const modalHero = document.querySelector('[data-modal-hero]');
+const modalPrograms1 = document.querySelector('[data-modal-programs1]');
+const modalPrograms2 = document.querySelector('[data-modal-programs2]');
+const modalPrograms3 = document.querySelector('[data-modal-programs3]');
 
 document.getElementById('form-hero').addEventListener('submit', function (event) {
     event.preventDefault();
+    modalHero.classList.toggle('is-hidden');
     
     let message = `<b>Кнопка - Головний екран</b>\n`;
     message += `<b>Ім'я: ${this.name.value}</b>\n`;
@@ -25,6 +31,7 @@ document.getElementById('form-hero').addEventListener('submit', function (event)
 
 document.getElementById('form-programs1').addEventListener('submit', function (event) {
     event.preventDefault();
+    modalPrograms1.classList.toggle('is-hidden');
     
     let message = `<b>Кнопка - Програма 1</b>\n`;
     message += `<b>Ім'я: ${this.name.value}</b>\n`;
@@ -46,6 +53,7 @@ document.getElementById('form-programs1').addEventListener('submit', function (e
 
 document.getElementById('form-programs2').addEventListener('submit', function (event) {
     event.preventDefault();
+    modalPrograms2.classList.toggle('is-hidden');
     
     let message = `<b>Кнопка - Програма 2</b>\n`;
     message += `<b>Ім'я: ${this.name.value}</b>\n`;
@@ -67,6 +75,7 @@ document.getElementById('form-programs2').addEventListener('submit', function (e
 
 document.getElementById('form-programs3').addEventListener('submit', function (event) {
     event.preventDefault();
+    modalPrograms3.classList.toggle('is-hidden');
     
     let message = `<b>Кнопка - Програма 2</b>\n`;
     message += `<b>Ім'я: ${this.name.value}</b>\n`;
